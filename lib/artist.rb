83a8b469049cc2a2e68221339d1c67333459687d
favorable-device-5158 <blee2125@gmail.com>
2019-09-25 14:41:27 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,4 +19,11 @@ class Artist
     song.artist = self
   end
 
+  def self.find_or_create_by_name(name)
+    self.find(name) ? self.find(name) : self.new(name)
+  end
+
+  def print_songs
+    songs.each {|song| puts song.name}
+  end
 end
